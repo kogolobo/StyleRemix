407a2c8f4c71a3ba4f09b33af2af09d8e25ffd9f
Konstantin Golobokov <kogolobo@uw.edu>
2025-02-16 20:37:04 -0800

    Clean up adapter training

--- new_style_tutorial/train_style_adapters.py
+++ new_style_tutorial/train_style_adapters.py
@@ -1,114 +1,39 @@
-"""
-Trains a LoRA adapter on a decoder-only LM to rewrite text towards a specific style dimension
-from the DiSC dataset (https://huggingface.co/datasets/hallisky/DiSC), such as 
-more sarcasm, less sarcasm, more formality, etc.
-
-Replace $HF_TOKEN with your huggingface token. You must have access to Meta-LLama-3-8B to run this.
-
-# Example to train LLama-3-8B to be more sarcastic
-CUDA_VISIBLE_DEVICES=0 python3 -m train_adapters.train_lora \
-    --model meta-llama/Meta-Llama-3-8B \
-    --hf_token $HF_TOKEN \
-    --lora_r 16 \
-    --lora_alpha 32 \
-    --lora_dropout 0.01 \
-    --data_path data_creation/generated_data_orig.json \
-    --style sarcasm_more;
-
-"""
-
-import os
-os.environ['TRANSFORMERS_CACHE'] = '../cache/'
-os.environ['HF_HOME'] = '../cache/'
-os.environ['HUGGINGFACE_HUB_CACHE'] = '../cache/'
-
-from trl import SFTTrainer, DataCollatorForCompletionOnlyLM
 import math
-
-from datetime import datetime
-time = datetime.now()    
-date_time = time.strftime("%m-%d-%Y_%H:%M:%S")
-
-from peft import (
-    LoraConfig,
-    PeftConfig,
-    TaskType,
-    PeftModel,
-    get_peft_model,
-)
-from transformers import (
-    AutoModelForCausalLM,
-    AutoTokenizer,
-    set_seed,
-    TrainingArguments
-)
+import os
+import pprint
+import numpy as np
 import torch
-from datasets import Dataset
-import argparse
-from huggingface_hub import login
-import json
-import sys
-from sklearn.model_selection import train_test_split
+from transformers import AutoTokenizer, AutoModelForCausalLM, set_seed
 from tokenizers.processors import TemplateProcessing
+from trl import SFTTrainer, DataCollatorForCompletionOnlyLM, SFTConfig
+from argparse import ArgumentParser
+from datasets import load_dataset
+from peft import LoraConfig, TaskType
 
-def main(args):
-    output_dir = os.path.join(args.output_dir,date_time)
-    os.makedirs(output_dir)
-    with open(os.path.join(output_dir, "args.json"), "w") as f:
-        json.dump(vars(args), f, indent=4)
-    print(f"\n\t*\tSaving to {output_dir}\n")
-    set_seed(args.seed)
+def train_adapter(dataset, direction, args):
+    dataset = dataset.map(
+        lambda x: {
+            "rewrite": x[f'{args.style}_{direction}'],
+        },
+        remove_columns=[f'{args.style}_less', f'{args.style}_more'],
+        num_proc=args.num_proc
+    )
+    dataset = dataset.train_test_split(test_size=args.test_ratio, seed=args.seed)
+    print(f"Train data size: {len(dataset['train'])}")
+    print(f"Test data size: {len(dataset['test'])}")
 
-    # Load the data
-    data = json.load(open(args.data_path, "r"))
-    print(f"List of available styles: {list(data.keys())}")
-    if args.style not in data.keys():
-        print("Passed in style not found in data. Exiting")
-        sys.exit()
-
-    all_origs = []
-    all_rewrites = []
-    for k in data[args.style]:        
-        all_origs.extend([c['content'] for c in data[args.style][k]['originals']])
-        all_rewrites.extend(data[args.style][k]['generations'])
-
-    # Split into train and dev set
-    train_data, eval_data = train_test_split(
-        list(zip(all_origs, all_rewrites)),
-        test_size=0.15, 
-        random_state=args.seed)
     
-    train_original, train_rewrite = zip(*train_data)
-    eval_original, eval_rewrite = zip(*eval_data)
-
-    train_dataset = Dataset.from_dict({
-        "original": train_original,
-        "rewrite": train_rewrite}
-        )
-    eval_dataset = Dataset.from_dict({
-        "original": eval_original,
-        "rewrite": eval_rewrite
-    })
-
-    # Set the huggingface token
-    if args.hf_token:
-        login(args.hf_token)
-    
-    device = 'cuda' if torch.cuda.device_count() > 0 else 'cpu'
-    print(f"\n\t*\tModels are on {device}\n")
-
-    # Initialize LoRA configuration for model adaptation    
+    device = "cuda" if torch.cuda.is_available() else "cpu"
     peft_config = LoraConfig(
         task_type=TaskType.CAUSAL_LM, 
         inference_mode=False, 
         r=args.lora_r, 
         lora_alpha=args.lora_alpha, 
         lora_dropout=args.lora_dropout,
-        bias = "none")
-
-    tokenizer = AutoTokenizer.from_pretrained(args.model, add_eos_token=True)
-
-    # Set up custom tokenization due to bug in LLama-3
+        bias = "none"
+    )
+    
+    tokenizer = AutoTokenizer.from_pretrained(args.model_name, use_fast=True)
     bos = tokenizer.bos_token
     eos = tokenizer.eos_token
     tokenizer._tokenizer.post_processor = TemplateProcessing(
@@ -126,154 +51,75 @@ def main(args):
     if not tokenizer.pad_token: # Set pad token if it doesn't exist
         tokenizer.add_special_tokens({'pad_token': '<padding_token>'})
 
-    """
-    Three ways to load peft config:
-    * AutoModelForCausalLM.from_pretrained(mmodel, peft_config = peft_config)
-    * PeftModel.from_pretrained(model, peft_config)
-    * peft.get_peft_model(model, peft_config)
-    """
-
-    steps = len(train_data)/(args.train_batch_size*torch.cuda.device_count())
-    save_steps = math.ceil(steps / args.save_ratio) # Save every quarter epoch
-    print(f"\n\t*\tSave steps is {save_steps}\n")
-
-    # Set up training arguments
-    training_args = TrainingArguments(
-        output_dir=output_dir,
+    model = AutoModelForCausalLM.from_pretrained(args.model_name)
+    model.resize_token_embeddings(len(tokenizer))
+    
+    out_path = f'./adapters/{args.style}_{direction}'
+    steps = len(dataset['train'])/(args.train_batch_size*torch.cuda.device_count())
+    save_steps = math.ceil(steps / args.save_ratio)
+    training_args = SFTConfig(
+        run_name=f"{args.style}_{direction}_adapter",
+        bf16=True,
+        learning_rate=args.lr,
+        output_dir=out_path,
+        logging_dir=os.path.join(out_path, 'logs'),
+        evaluation_strategy='steps',
+        eval_steps=save_steps,
+        save_steps=save_steps,
+        logging_steps=save_steps,
+        num_train_epochs=args.num_train_epochs,
         per_device_train_batch_size=args.train_batch_size,
         per_device_eval_batch_size=args.eval_batch_size,
-        report_to="tensorboard",
-        evaluation_strategy='steps',
-        num_train_epochs=args.epochs,
-        eval_steps = save_steps,
-        save_steps = save_steps,
-        logging_steps = save_steps,
-        lr_scheduler_type = 'linear',
-        seed = args.seed,
-        warmup_ratio = 0.1,
-        packing = False,
-        max_seq_length = args.max_seq_length
+        load_best_model_at_end=True,
+        save_total_limit=1,
+        lr_scheduler_type='linear',
+        warmup_ratio=0.1,
+        packing=False,
+        max_seq_length=args.max_seq_length
     )
 
-    # Define a function to format prompts for training and the response format
-    def formatting_prompts_func(example):
-        output_texts = []
-        for i in range(len(example['original'])):
-            text = f"### Original: {example['original'][i]}\n ### Rewrite: {example['rewrite'][i]}"
-            output_texts.append(text)
-        return output_texts
     response_template = " ### Rewrite:"
-    collator = DataCollatorForCompletionOnlyLM(response_template, tokenizer=tokenizer)
-
-    model = AutoModelForCausalLM.from_pretrained(args.model)
-    model.resize_token_embeddings(len(tokenizer)) # Resize to add pad token. Value doesn't matter
+    formatting_prompts_func = lambda example: f"### Original: {example['original']}\n{response_template} {example['rewrite']}"
+    collator = DataCollatorForCompletionOnlyLM(response_template, tokenizer=tokenizer)    
     trainer = SFTTrainer(
         model,
         args=training_args,
-        train_dataset=train_dataset,
-        eval_dataset=eval_dataset,
-        # dataset_text_field="text",
+        train_dataset=dataset['train'],
+        eval_dataset=dataset['test'],
         formatting_func=formatting_prompts_func,
-        # packing=False,
-        # max_seq_length=args.max_seq_length,
         peft_config=peft_config,
         tokenizer=tokenizer,
         data_collator=collator
     )
-    print("\n")
-    trainer.model.print_trainable_parameters() # Print out the trainable parameters
-    assert trainer.train_dataset.num_rows == len(train_data)
-    print(tokenizer.decode(trainer.train_dataset[0]['input_ids']))
 
-    trainer.train() # Train the model 
+    trainer.train()
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument("--model_name", type=str, default="meta-llama/Meta-Llama-3-8B")
+    parser.add_argument("--style", type=str, default="sarcasm")
+    parser.add_argument("--test_ratio", type=float, default=0.15)
+    parser.add_argument("--num_proc", type=int, default=4)
+    parser.add_argument("--seed", type=int, default=42)
+    
+    parser.add_argument("--lora_r", type=int, default=16)
+    parser.add_argument("--lora_alpha", type=float, default=32.0)
+    parser.add_argument("--lora_dropout", type=float, default=0.01)
+    
+    parser.add_argument("--train_batch_size", type=int, default=6)
+    parser.add_argument("--eval_batch_size", type=int, default=6)
+    parser.add_argument("--max_seq_length", type=int, default=512)
+    parser.add_argument("--lr", type=float, default=5e-5)
+    parser.add_argument("--num_train_epochs", type=int, default=8)
+    parser.add_argument("--save_ratio", type=int, default=4)
+    args = parser.parse_args()
+    pprint.pprint(vars(args))
+
+    set_seed(args.seed)
+    dataset = load_dataset("json", data_files=f"/home/kogolobo/repos/StyleRemix/new_style_tutorial/{args.style}_classifier_examples.jsonl")['train'].remove_columns(['book_id', 'title', 'date'])
+    
+    for direction in ["less", "more"]:
+        train_adapter(dataset, direction, args)    
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Train a LoRA adapter on a decoder-only language model (LM) to rewrite text towards a specific style dimension from the DiSC dataset.")
-
-    parser.add_argument('--model', type=str, default="meta-llama/Meta-Llama-3-8B")
-
-    parser.add_argument(
-        '--output_dir', 
-        type=str, 
-        default="style_training/outputs",
-        help="Directory to save the trained model and other outputs."
-    )
-
-    parser.add_argument(
-        '--hf_token', 
-        type=str, 
-        default=None,
-        help="Your Hugging Face token to access private models. Replace $HF_TOKEN with your token."
-    )
-
-    parser.add_argument(
-        '--lora_r', 
-        type=int, 
-        default=16,
-        help="LoRA rank, which defines the number of low-rank adaptations."
-    )
-
-    parser.add_argument(
-        '--lora_alpha', 
-        type=int, 
-        default=32,
-        help="LoRA alpha, a scaling factor for LoRA updates."
-    )
-
-    parser.add_argument(
-        '--lora_dropout', 
-        type=float, 
-        default=0.01,
-        help="Dropout rate for LoRA layers."
-    )
-
-    parser.add_argument(
-        '--style', 
-        type=str, 
-        default='sarcasm',
-        help="Style dimension to train the model on. Available styles include sarcasm_more, sarcasm_less, formality_informal, formality_formal, etc."
-    )
-
-    parser.add_argument(
-        '--seed', 
-        type=int, 
-        default=0,
-        help="Random seed for reproducibility."
-    )
-
-    parser.add_argument(
-        '--save_ratio', 
-        type=int, 
-        default=4,
-        help="Ratio of the total training steps to save checkpoints (e.g., 4 saves every quarter epoch)."
-    )
-
-    parser.add_argument(
-        '--epochs', 
-        type=int, 
-        default=7,
-        help="Number of training epochs."
-    )
-
-    parser.add_argument(
-        '--train_batch_size', 
-        type=int, 
-        default=6,
-        help="Batch size for training."
-    )
-
-    parser.add_argument(
-        '--eval_batch_size', 
-        type=int, 
-        default=4,
-        help="Batch size for evaluation."
-    )
-
-    parser.add_argument(
-        '--max_seq_length', 
-        type=int, 
-        default=512,
-        help="Maximum sequence length for the input text."
-    )
-
-    main(parser.parse_args())
+    main()
